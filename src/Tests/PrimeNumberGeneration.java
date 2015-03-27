47e2aacf90109afe040ccc1cd7e0dba648e3ba1c
jarvisoa <jarvisoa@rose-hulman.edu>
2015-03-27 02:32:15 -0400

    added first test to prime number generator

---
+++ src/Tests/PrimeNumberGeneration.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumberGeneration {
+
+	public static List<Integer> generate(int i) {
+		List<Integer> primes = new ArrayList<Integer>();
+		return primes;
+	}
+}
---
+++ src/Tests/PrimeNumberGenerationTest.java
@@ -0,0 +1,54 @@
+package Tests;
+
+
+	import java.util.Arrays;
+import java.util.Collection;
+	 
+import java.util.List;
+
+	import org.junit.Test;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+	@RunWith(Parameterized.class)
+public class PrimeNumberGenerationTest {
+	   private Integer inputNumber;
+	   private List<Integer> expectedResult;
+	   private PrimeNumberGeneration primeNumberChecker;
+
+	   @Before
+	   public void initialize() {
+	      primeNumberChecker = new PrimeNumberGeneration();
+	   }
+
+	   // Each parameter should be placed as an argument here
+	   // Every time runner triggers, it will pass the arguments
+	   // from parameters we defined in primeNumbers() method
+	   public PrimeNumberGenerationTest(Integer inputNumber, 
+	      List<Integer> expectedResult) {
+	      this.inputNumber = inputNumber;
+	      this.expectedResult = expectedResult;
+	   }
+	   
+	   private static List<Integer> list(Integer ...integers) {
+			return Arrays.asList(integers);
+		}
+
+	   @Parameterized.Parameters
+	   public static Collection primeNumbers() {
+	      return Arrays.asList(new Object[][] {
+	         { 2, list(1)},
+	      });
+	   }
+
+	   @Test
+	   public void testPrimeNumberChecker() {
+	      System.out.println("Parameterized Number is : " + inputNumber);
+	      assertEquals(expectedResult, 
+	      primeNumberChecker.generate(inputNumber));
+	   }
+	}
