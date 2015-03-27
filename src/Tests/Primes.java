be5c899b85adc4a6f987d596aa6b371692cfea3f
jarvisoa <jarvisoa@rose-hulman.edu>
2015-03-27 02:12:55 -0400

    passed testNine

--- src/Tests/Primes.java
+++ src/Tests/Primes.java
@@ -7,9 +7,12 @@ public class Primes {
 
 	public static List<Integer> getPrimeFactors(int i) {
 		List<Integer> primes = new ArrayList<Integer>();
-		while(i % 2 == 0){
-			primes.add(2);
-			i /= 2;
+		int divisor = 2;
+		while(i > 1){
+			for(;i % divisor == 0;i/=divisor){
+				primes.add(divisor);
+			}
+			divisor++;
 		}
 		if (i > 1)
 			primes.add(i);
