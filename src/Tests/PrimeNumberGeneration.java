386e7c2f803bac3449f60a4d402fad47244f1365
jarvisoa <jarvisoa@rose-hulman.edu>
2015-03-27 02:35:12 -0400

    passed test of 3

--- src/Tests/PrimeNumberGeneration.java
+++ src/Tests/PrimeNumberGeneration.java
@@ -8,6 +8,9 @@ public class PrimeNumberGeneration {
 	public static List<Integer> generate(int i) {
 		List<Integer> primes = new ArrayList<Integer>();
 		primes.add(1);
+		if(i>2){
+			primes.add(2);
+		}
 		return primes;
 	}
 }
